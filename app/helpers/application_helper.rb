413a1cfc9ca42918853c2f6668eb8fdfb1700459
Andrey Mukhametzyanov <FSzV3S7um9ecUg4GECs@example.com>
2026-07-01 02:23:42 +0400

    add error render

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def form_control_class(object, attribute, base_class: "form-control")
+    [ base_class, ("is-invalid" if object.errors[attribute].any?) ].compact.join(" ")
+  end
 end
